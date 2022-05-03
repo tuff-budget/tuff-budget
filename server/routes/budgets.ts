83f392184e4a9f3ee4146b682e4abed4752d57bf
Judy Song <judysongg@gmail.com>
2022-05-02 17:34:54 -0700

    boilerplate

---
+++ server/routes/budgets.ts
@@ -0,0 +1,12 @@
+// const express = require('express');
+// const router = express.Router();
+
+// // IMPORT CONTROLLERS
+// const budgetsController = '../controllers/budgetsController';
+
+// // REQUEST HANDLERS
+
+
+
+// // EXPORT ROUTER
+// module.exports = router;
